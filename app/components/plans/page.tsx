d07033b7e83f363dfb7ec4b6bcbb2596334b2b2f
bdhamithkumara <damithkumararoxx55@gmail.com>
2024-04-28 13:50:31 +0530

    completed plan

--- app/components/plans/page.tsx
+++ app/components/plans/page.tsx
@@ -1,9 +1,130 @@
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
-    <div>plans</div>
+    <section className='w-full flex flex-col'>
+      <div className='flex justify-center flex-col'>
+        <div className='lg:mx-20 mx-5 max-w-[1536px] flex-col'>
+          <div className='flex flex-col items-center lg:pt-[60px] pt-[48px]'>
+            <h3 className='md:text-[42px] text-[24px] font-semibold w-auto text-center leading-tight text-[#172026]'>Flexible plans for you</h3>
+            <p className='mt-[16px] lg:text-[18px] text-[16px] w-auto text-center text-[#36485c]'>No hidden fees!</p>
+          </div>
+
+          <div className='mt-[64px] flex justify-between w-full gap-6 plans-col-row items-center md:items-stretch'>
+
+            <div className='bg-[#f5f4ff] rounded-lg xl:p-6 p-4 md:w-[40%] sm:w-[50%] xs:w-[80%] w-[100%] flex flex-col justify-between'>
+              <div>
+                <p className='text-[#4228eb] font-semibold text-[20px]'>Free Trial</p>
+                <p className='text-[#36485c] xl:text-[18px] lg:text-[17px] mt-[16px]'>Perfect for testing the waters</p>
+                <span className='text-[32px] font-semibold mt-[32px]'>
+                  <span className='text-[#172026]'>$0</span>
+                  <span className='text-[#567896]'>/mo</span>
+                </span>
+                <p className='text-[#36485c] mt-[32px] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#36485C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Lorem ipsum dolor sit amet
+                </p>
+                <p className='text-[#36485c] my-[16px] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#36485C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Sed do eiusmod tempor incididunt
+                </p>
+                <p className='text-[#36485c]  flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#36485C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Consectetur adipiscing elit
+                </p>
+              </div>
+              <button className='mt-[32px] w-[100%] bg-[#fff] py-[14px] rounded'>
+                <span className='text-[#4328eb] font-semibold text-[16px]'>Start Trial</span>
+              </button>
+            </div>
+
+            <div className='bg-[#4328eb] rounded-lg xl:p-6 p-4 md:w-[40%] sm:w-[50%] xs:w-[80%] w-[100%] flex flex-col justify-between'>
+              <div>
+              <p className='text-[#fff] font-semibold text-[20px]'>Business</p>
+              <p className='text-[#f4f8fa] xl:text-[18px] lg:text-[17px] mt-[16px]'>Perfect for small businesses</p>
+              <span className='text-[32px] font-semibold text-[#fff] mt-[32px]'>$500/mo</span>
+              
+              <p className='text-[#fff] mt-[32px] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Lorem ipsum dolor sit amet
+                </p>
+
+                <p className='text-[#FFF] my-[16px] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Consectetur adipiscing elit
+                </p>
+
+                <p className='text-[#FFF]  flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Sed do eiusmod tempor incididunt
+                </p>
+
+                <p className='text-[#FFF] my-[16px] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Lorem ipsum dolor sit amet
+                </p>
+
+                <p className='text-[#FFF] flex text-[16px]'>
+                  <span className='my-auto mr-[16px]'>                    
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 6L9 17L4 12" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                  </span>
+                  Consectetur adipiscing elit
+                </p>
+            </div>
+            <button className='mt-[32px] w-[100%] bg-[#fff] py-[14px] rounded'>
+                <span className='text-[#4328eb] font-semibold text-[16px]'>Get Started</span>
+              </button>
+            </div>
+
+            <div className='bg-[#f5f4ff] rounded-lg xl:p-6 p-4 md:w-[40%] sm:w-[50%] xs:w-[80%] w-[100%] flex flex-col justify-between'>
+              <div>
+                <p className='text-[#4328eb] font-semibold text-[20px]'>Enterprise</p>
+                <p className='text-[#36485c] text-[18px] mt-[16px]'>Perfect for big companies</p>
+                <span className='text-[32px] font-semibold text-[#172026] mt-[32px]'>Custom</span>
+                <p className='text-[#36485c] mt-[32px] text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. </p>
+                <p className='text-[#36485c] mt-[16px] text-[16px]'>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+              <button className='mt-[32px] w-[100%] bg-[#fff] py-[14px] rounded'>
+                <span className='text-[#4328eb] font-semibold text-[16px]'>Contact Us</span>
+              </button>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default page
+export default Page
